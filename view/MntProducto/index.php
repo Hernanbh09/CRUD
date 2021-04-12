1bfa5f30676804791404716c019da32ba5b6e44b
hernanbh09 <hernandezbrayan697@gmail.com>
2021-04-12 00:53:48 -0500

    12/02/2021-12:53A.M V1.1

---
+++ view/MntProducto/index.php
@@ -0,0 +1,132 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" href="../../css/principal.css">
+  <script src="https://kit.fontawesome.com/c1eae85dfa.js" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+
+  <!-- vendor css -->
+    <link href="../../public/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="../../public/lib/Ionicons/css/ionicons.css" rel="stylesheet">
+    <link href="../../public/lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+    <link href="../../public/lib/jquery-switchbutton/jquery.switchButton.css" rel="stylesheet">
+
+    <link href="../../public/lib/datatables/jquery.dataTables.css" rel="stylesheet">
+    <link href="../../public/datatables/buttons.dataTables.min.css" rel="stylesheet"/>
+
+    <!-- Bracket CSS -->
+    <link rel="stylesheet" href="../../public/css/bracket.css">
+
+    
+  <title>&copy; Capital Telas</title>
+</head>
+
+<body>
+
+
+
+  <header>
+
+    <div>
+
+      <nav class="menu">
+        <img src="../../img/logo.png" alt="logo" class="logo">
+        <ul class="items">
+          <li><a href="" hreh="#">Welcome</a></li>
+          <li><a href="" hreh="#">help<i class="far fa-question-circle"></i></a></li>
+          <li><a href="#">shopping</a></li>
+          
+          <!-- <li class="nombre"><a href="#"><?php echo $_SESSION['username']; ?> </i></a></li> -->
+          
+          <li class="active"><a href="cerrar.php">Sign out <i class="fas fa-sign-out-alt"></i></a></li>
+        </ul>
+
+        <span class="botton">
+          <i class="fa fa-bars "></i>
+        </span>
+
+      </nav>
+    </div>
+
+  </header>
+
+
+
+
+  <main>
+  <div class="br-mainpanel">
+      <div class="br-pageheader pd-y-15 pd-l-20">
+        <nav class="breadcrumb pd-0 mg-0 tx-12">
+          <a class="breadcrumb-item" href="index.html">Mantenimiento</a>
+          <span class="breadcrumb-item active">Producto</span>
+        </nav>
+      </div><!-- br-pageheader -->
+      <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+        <h4 class="tx-gray-800 mg-b-5">Producto</h4>
+        <p class="mg-b-0">Desde esta ventana podra dar mantenimiento a productos</p>
+      </div>
+
+      <div class="br-pagebody">
+        <div class="br-section-wrapper">
+            <h6 class="tx-gray-800 tx-uppercase tx-bold tx-14 mg-b-10">Mantenimiento de Producto</h6>
+            <button id="btnnuevo" class="btn btn-outline-primary btn-block mg-b-10">Nuevo Registro</button>
+
+            <div class="table-wrapper">
+              <table id="producto_data" class="table display responsive nowrap">
+                <thead>
+                  <tr>
+                    <th class="wd-15p">Nombre</th>
+                    <th class="wd-15p"></th>
+                    <th class="wd-20p"></th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+              </table>
+            </div>
+        </div>
+      </div>
+
+    </div><!-- br-mainpanel -->
+    <!-- ########## END: MAIN PANEL ########## -->
+
+
+
+
+   
+  
+  
+  </main>
+
+    <?php require_once("modalmantenimiento.php");  ?>
+
+    <script src="../../public/lib/jquery/jquery.js"></script>
+    <script src="../../public/lib/popper.js/popper.js"></script>
+    <script src="../../public/lib/bootstrap/bootstrap.js"></script>
+    <script src="../../public/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
+    <script src="../../public/lib/moment/moment.js"></script>
+    <script src="../../public/lib/jquery-ui/jquery-ui.js"></script>
+    <script src="../../public/lib/jquery-switchbutton/jquery.switchButton.js"></script>
+    <script src="../../public/lib/peity/jquery.peity.js"></script>
+    <script src="../../public/js/bracket.js"></script>
+
+    <script src="../../public/lib/datatables/jquery.dataTables.js"></script>
+    <script src="../../public/lib/datatables-responsive/dataTables.responsive.js"></script>
+
+    <script src="../../public/datatables/dataTables.buttons.min.js"></script>
+    <script src="../../public/datatables/buttons.html5.min.js"></script>
+    <script src="../../public/datatables/buttons.colVis.min.js"></script>
+    <script src="../../public/datatables/jszip.min.js"></script>
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+    <script type="text/javascript" src="mntproducto.js"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+
+</html>
